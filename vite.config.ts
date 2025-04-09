3d5bd34ba851a71bdcee57b57b07cea20c2ac987
JorgeAlbertoLopezHdz <jorgealberto.lopezhdz@gmail.com>
2025-04-08 20:37:41 -0600

    fix: fixed vite base for Vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Bluewave-cs.github.io/blue-wave'
+  base: '/'
 })
